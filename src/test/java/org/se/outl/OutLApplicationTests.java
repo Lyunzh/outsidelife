eb10965b576db7b38be585fe6372bd89e7772939
Lyunzh <yunzhi070@gmail.com>
2024-12-27 20:50:47 +0800

    初始化项目

---
+++ src/test/java/org/se/outl/OutLApplicationTests.java
@@ -0,0 +1,13 @@
+package org.se.outl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
