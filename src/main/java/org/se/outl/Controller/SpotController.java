813d13431e6b39be824b81a198a88da59d698f51
Lyunzh <yunzhi070@gmail.com>
2024-12-28 21:10:19 +0800

    homepage

---
+++ src/main/java/org/se/outl/Controller/SpotController.java
@@ -0,0 +1,44 @@
+package org.se.outl.Controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.se.outl.Entity.Spot;
+import org.se.outl.Service.SpotService;
+import org.se.outl.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/spot")
+public class SpotController {
+
+    @Autowired
+    private SpotService spotService;
+
+    @GetMapping("/list")
+    public Result getSpot(@RequestHeader("Authorization") String token) {
+        if(token == null) {
+            return Result.error("请先登录");
+        }
+        return Result.success(spotService.getRecentSpot());
+    }
+
+    @GetMapping("/hike")
+    public Result getHike(@RequestHeader("Authorization") String token) {
+        if(token == null) {
+            return Result.error("请先登录");
+        }
+        return Result.success(spotService.getHikeSpot());
+    }
+
+    @GetMapping("/bike")
+    public Result getBike(@RequestHeader("Authorization") String token) {
+        if(token == null) {
+            return Result.error("请先登录");
+        }
+        return Result.success(spotService.getBikeSpot());
+    }
+}
---
+++ src/main/java/org/se/outl/Service/impl/SpotServiceimpl.java
@@ -0,0 +1,35 @@
+package org.se.outl.Service.impl;
+
+
+import org.se.outl.Mapper.SpotMapper;
+import org.se.outl.Service.SpotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.se.outl.Entity.Spot;
+
+import java.util.List;
+
+@Service
+public class SpotServiceimpl implements SpotService {
+    @Autowired
+    private SpotMapper spotMapper;
+
+    @Override
+    public List<Spot> getRecentSpot()
+    {
+        return spotMapper.getRecentSpot();
+    }
+
+    @Override
+    public List<Spot> getBikeSpot(){
+        return spotMapper.getBikeSpot();
+    }
+
+    @Override
+    public List<Spot> getHikeSpot()
+    {
+        return spotMapper.getHikeSpot();
+    }
+
+
+}
